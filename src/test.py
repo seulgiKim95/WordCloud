406e8b886e0ff7a522c3537813ed3c47f14fcf8e
Seulgi Kim <ksglm1231@gmail.com>
2022-08-03 10:00:15 +0900

    update src

--- src/test.py
+++ src/test.py
@@ -3,21 +3,15 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
-import os
-from selenium.webdriver.common.by import By
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')        ## Head-less 설정
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--disable-gpu')
-options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36')
-chromedriver = '../data/chromedriver.exe'
+driver = webdriver.Chrome('chromedriver', options=options)
 
-base_url = "https://www.saramin.co.kr/zf_user/search?searchword="
-driver = webdriver.Chrome(chromedriver, options=options)
+
+base_url = "https://www.rocketpunch.com/jobs"
+data_scientist = "데이터 사이언티스트"
+
 driver.get(base_url)
-time.sleep(10);
-html = driver.page_source
-#rint (html)
-driver.quit()
